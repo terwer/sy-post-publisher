56fd048420c2314149f865b791ec5cf2a56c2ee9
terwer <youweics@163.com>
2023-05-09 17:53:03 +0800

    refactor: 迁移源码到挂件仓库 https://github.com/siyuan-note/bazaar/issues/1095

--- static/js/vendor_is-plain-object-eb550d51.js
+++
@@ -1,7 +0,0 @@
-/*!
- * is-plain-object <https://github.com/jonschlinkert/is-plain-object>
- *
- * Copyright (c) 2014-2017, Jon Schlinkert.
- * Released under the MIT License.
- */
-function t(t){return"[object Object]"===Object.prototype.toString.call(t)}function o(o){var r,e;return!1!==t(o)&&(void 0===(r=o.constructor)||!1!==t(e=r.prototype)&&!1!==e.hasOwnProperty("isPrototypeOf"))}export{o as i};
